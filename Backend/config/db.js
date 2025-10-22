7ae171bf6195b572d1ad8e846ea46765e4eac75f
kavyakapoor420 <kavyakapoor412@gmail.com>
2025-10-22 21:27:03 +0530

    hardcoded mongodb_uri

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,9 @@ const dotenv=require('dotenv')
 
 dotenv.config() 
 
-const MONGO_URI = process.env.MONGO_URI;
+// const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI='mongodb+srv://kavyakapoor413:oreokapoor@cluster01.4zpagwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01'
+
 
 
 const connectDb=async ()=>{
